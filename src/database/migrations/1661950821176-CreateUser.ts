da57cbe6b97d97c56479857daa0d7985e27af56c
Pedro landim <henriquep35036@gmail.com>
2022-09-04 01:16:48 -0300

    Criação das Migrations

---
+++ src/database/migrations/1661950821176-CreateUser.ts
@@ -0,0 +1,39 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUser1661950821176 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "user",
+                columns : [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                        isUnique: true
+                    },
+                    {
+                        name: "description",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+    
+    //Serve para reverter a criação da Migrations
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable("user");
+    }
+
+}
